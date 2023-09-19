bdf929fc21da7b3cfaa58484c1c00bf91afa4138
upsadin <upsadin@gmail.com>
2023-09-19 21:39:06 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,33 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link);
+    }
+}
